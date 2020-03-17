91e549185793cc60acaab81533fc2e6e2518b518
sopalab <40566530+sopalab@users.noreply.github.com>
2020-03-17 11:56:54 +0100

    Minor changes.

--- YunTemp/connectiontable_yuntemp.py
+++ YunTemp/connectiontable_yuntemp.py
@@ -1,13 +1,11 @@
-
 from labscript import *
 
 from labscript_devices import CustomArduinoDevice
-from labscript_devices.DummyPseudoclock.labscript_device import DummyPseudoclock
+from labscript_devices.DummyPseudoclock.labscript_devices import DummyPseudoclock
 
 DummyPseudoclock(name='clock_0')
-CustomArduinoDevice(name='arduino_0', parent_device=clock_0.clockline,
-    com_port="/dev/ttys004")
+CustomArduinoDevice(name='arduino_0', parent_device=clock_0.clockline, com_port="/dev/ttys004")
 
-if __name__ == '__main__':
-    start()
-    stop(1)
+#if __name__ == '__main__':
+start()
+stop(1)
