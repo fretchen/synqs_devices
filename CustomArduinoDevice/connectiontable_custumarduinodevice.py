c284e8a586c88d382caf765a6c78dd17d38977dc
Fred J <fred.jendrzejewski@gmail.com>
2020-03-19 14:53:01 +0100

    Update to a new connection table to integrate the YunTemp

--- CustomArduinoDevice/connectiontable_custumarduinodevice.py
+++ CustomArduinoDevice/connectiontable_custumarduinodevice.py
@@ -1,11 +1,8 @@
 from labscript import *
 
 from user_devices.CustomArduinoDevice.labscript_devices import CustomArduinoDevice
-from labscript_devices.DummyPseudoclock.labscript_devices import DummyPseudoclock
 
-DummyPseudoclock(name='clock_0')
 CustomArduinoDevice('arduino_0', com_port="/dev/ttys004")
 
-#if __name__ == '__main__':
 start()
 stop(1)
--- YunTemp/blacs_workers.py
+++ YunTemp/blacs_workers.py
@@ -28,7 +28,7 @@ class Mako_Camera(object):
         global Vimba
         global VimbaException
         from pymba import Vimba, VimbaException
-        
+
         self.data=[]
         self.frames=[]
         self.itr=0
@@ -41,11 +41,11 @@ class Mako_Camera(object):
         #pos
         self.camera = vimba.camera(Camera_ID)#vimba.camera_ids()[serial_number])#Device id.
         self.camera.open(camera_access_mode = 1)
-        
+
     def set_attributes(self, attributes_dict):
         for prop, vals in attributes_dict.items():
             self.set_attribute(prop, vals)
-    
+
     def set_attribute(self, name, value):
         """Set the value of the attribute of the given name to the given value"""
         try:
@@ -54,7 +54,7 @@ class Mako_Camera(object):
             print('failed to set {name} to {value}')
         #feature = self.camera.feature(name)
         #feature.value = value
-    
+
     def get_attributes(self, visibility_level, writeable_only=True):
         """Return a dict of all attributes of readable attributes, for the given
         visibility level. Optionally return only writeable attributes.
@@ -63,7 +63,7 @@ class Mako_Camera(object):
         features=self.camera.feature_names()
         for feature in features:
             props[feature]=self.get_attribute(feature)
-        
+
         del props['AcquisitionAbort'], props['AcquisitionStart'], props['AcquisitionStop'], props['GVSPAdjustPacketSize'], props['GevTimestampControlLatch'], props['GevTimestampControlReset'], props['LUTLoadAll'], props['LUTSaveAll'], props['TriggerSoftware'], props['UserSetLoad'], props['UserSetSave']
         return props
 
@@ -87,7 +87,7 @@ class Mako_Camera(object):
             self.camera.AcquisitionStop()
             self.camera.disarm()
         return img
-        
+
     def configure_acquisition(self, continuous=True, bufferCount=7):
         mako_attributes={'AcquisitionMode':'Continuous', 'ExposureMode':'Timed', 'ExposureTimeAbs':3000, 'TriggerActivation':'RisingEdge', 'TriggerMode':'Off',  'TriggerSelector':'FrameStart', 'TriggerSource':'Freerun'}
         self.set_attributes(mako_attributes)
@@ -95,23 +95,23 @@ class Mako_Camera(object):
             self.camera.AcquisitionMode='Continuous'
             one=True
             self.frames=[self.camera.new_frame() for _ in range(bufferCount)]#Make a frame buffer.
-        
+
             for self.frame in self.frames:
                 self.frame.announce()
                 if one:
                     self.camera.start_capture()
                     one=False
                 self.frame.queue_for_capture()
-            
+
             self.camera.AcquisitionStart()
-        
+
         else:
             self.camera.TriggerMode = 'On'
             self.camera.TriggerSource = 'Line1'
             self.camera.AcquisitionMode='MultiFrame'
             self.camera.ExposureMode = 'TriggerWidth'
-            
-                        
+
+
     def grab(self):
         """Grab and return single image during pre-configured acquisition."""
         self.frames[self.itr].wait_for_capture(1000)
@@ -120,38 +120,38 @@ class Mako_Camera(object):
         self.itr+=1
         if self.itr==len(self.frames):
             self.itr=0
-        
+
         return self.data
-        
+
     def grab_multiple(self, n_images,images):
         """Grab n_images into images array during buffered acquistion. Length of exposure is controlled by the hardware TTL trigger duration"""
         self.frames=[self.camera.new_frame() for _ in range(n_images)]#Make a frame buffer.
         for self.frame in self.frames:
-            self.frame.announce()                    
-        self.camera.start_capture()        
+            self.frame.announce()
+        self.camera.start_capture()
         for i in range(n_images):
             self.frames[i].queue_for_capture()
             self.camera.AcquisitionStart()
             self.frames[i].wait_for_capture(21000)#in ms
             images.append(self.frames[i].buffer_data_numpy())
             self.camera.AcquisitionStop()
-   
+
     def stop_acquisition(self):
         self.camera.AcquisitionStop()
         self.camera.disarm()
 
     def abort_acquisition(self):
         self.camera.AcquisitionAbort()
-        
+
     def close(self):
         self.camera.disarm()
         self.camera.close()
-        
+
 
 class MakoCameraWorker(IMAQdxCameraWorker):
-    """Mako API Camera Worker. 
-    
-    Inherits from IMAQdxCameraWorker. Overloads get_attributes_as_dict 
+    """Mako API Camera Worker.
+
+    Inherits from IMAQdxCameraWorker. Overloads get_attributes_as_dict
     to use Mako_Camera.get_attributes() method."""
     interface_class = Mako_Camera
 
@@ -159,5 +159,3 @@ class MakoCameraWorker(IMAQdxCameraWorker):
         """Return a dict of the attributes of the camera for the given visibility
         level"""
         return self.camera.get_attributes(visibility_level)
-
-
