e4117b5f247fb3d83153e9b9280a976db4b15a82
Fred J <fred.jendrzejewski@gmail.com>
2020-03-20 17:21:46 +0100

    Update blacs_tabs.py
    Able to get some widgets, but get some weird init error, I cannot track down.

--- YunTemp/blacs_tabs.py
+++ YunTemp/blacs_tabs.py
@@ -13,24 +13,65 @@ class YunTempTab(DeviceTab):
         settings: Not sure here.
         worker: Not sure here.
     """
-    def initialise_workers(self):
 
-        #  create the layout of the widgets
-        dds_widgets, ao_widgets, do_widgets = self.auto_create_widgets()
-        self.auto_place_widgets(dds_widgets, do_widgets)
+    def initialise_GUI(self):
+        """Define the layout of the tab.
 
-        # Look up the COM port and baud rate in the connection table:
-        connection_table = self.settings["connection_table"]
-        device = connection_table.find_by_name(self.device_name)
+        Returns:
+            Nothing really. Just sets things up.
+        """
+    # Capabilities
+        self.base_units =    {'freq':'Hz',               'phase':'Degrees'}
+        self.base_min =      {'freq':200e3,              'phase':0}
+        self.base_max =      {'freq':402.653183*10.0**6, 'phase':360}
+        self.base_step =     {'freq':10**6,              'phase':1}
+        self.base_decimals = {'freq':0,                  'phase':3} # TODO: find out what the phase precision is!
+        self.num_DDS = 1
 
-        com_port = device.properties["com_port"]
-        baud_rate = device.properties["baud_rate"]
+        # Create DDS Output objects
+        dds_prop = {}
+        for i in range(self.num_DDS): # only 1 DDS output
+            dds_prop['channel %d' % i] = {}
+            for subchnl in ['freq', 'phase']:
+                dds_prop['channel %d' % i][subchnl] = {'base_unit':self.base_units[subchnl],
+                                                     'min':self.base_min[subchnl],
+                                                     'max':self.base_max[subchnl],
+                                                     'step':self.base_step[subchnl],
+                                                     'decimals':self.base_decimals[subchnl]
+                                                    }
+        # Create the output objects
+        self.create_dds_outputs(dds_prop)
+        # Create widgets for output objects
+        dds_widgets,ao_widgets,do_widgets = self.auto_create_widgets()
+        # and auto place the widgets in the UI
+        self.auto_place_widgets(("DDS Outputs",dds_widgets))
 
-        # Start a worker process with our worker class, and pass it the com port and
-        # baud rate, which it will need:
-        self.create_worker(
-            "main_worker",
-            "user_devices.YunTemp.blacs_workers.YunTempWorker",
-            {"com_port": com_port, "baud_rate": baud_rate},
-        )
+        connection_object = self.settings['connection_table'].find_by_name(self.device_name)
+        conn_properties = connection_object.properties
+
+        # Store the COM port to be used
+        blacs_connection =  str(connection_object.BLACS_connection)
+        if ',' in blacs_connection:
+            self.com_port, baud_rate = blacs_connection.split(',')
+            self.baud_rate = int(baud_rate)
+        else:
+            self.com_port = blacs_connection
+            self.baud_rate = 19200
+
+        self.ext_clk = conn_properties.get('ext_clk',False)
+        self.clk_freq = conn_properties.get('clk_freq', None)
+        self.clk_scale = conn_properties.get('clk_scale',1)
+
+        # Create and set the primary worker
+        self.create_worker("main_worker","user_devices.YunTemp.blacs_workers.YunTempWorker",
+                                {'com_port':self.com_port,
+                                'baud_rate': self.baud_rate,
+                                'ext_clk': self.ext_clk,
+                                'clk_freq': self.clk_freq,
+                                'clk_scale': self.clk_scale
+                                })
         self.primary_worker = "main_worker"
+
+        # Set the capabilities of this device
+        self.supports_remote_value_check(True)
+        self.supports_smart_programming(True)
